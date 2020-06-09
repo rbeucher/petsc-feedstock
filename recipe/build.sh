84fd06955ffc66186728f2482e38a6b98973364f
Romain Beucher <romain.beucher@anu.edu.au>
2020-06-09 11:49:16 +1000

    test

--- recipe/build.sh
+++ recipe/build.sh
@@ -58,7 +58,7 @@ python ./configure \
   --with-blas-lib=libblas${SHLIB_EXT} \
   --with-lapack-lib=liblapack${SHLIB_EXT} \
   --with-hdf5=1 \
-  --with-hdf5-lib=[libhdf5${SHLIB_EXT}, libhdf5_hl${SHLIB_EXT}, libhdf5hl_fortran${SHLIB_EXT}, libhdf5_fortran${SHLIB_EXT}] \
+  --with-hdf5-lib=[libhdf5${SHLIB_EXT}, libhdf5-hl${SHLIB_EXT}, libhdf5hl-fortran${SHLIB_EXT}, libhdf5-fortran${SHLIB_EXT}] \
   --with-hwloc=0 \
   --with-hypre=1 \
   --with-metis=1 \
