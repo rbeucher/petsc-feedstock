6f3f05d0d42683115dcdc24d9149dc4ff5f53a2e
Romain Beucher <romain.beucher@anu.edu.au>
2020-06-10 10:16:12 +1000

    test

--- recipe/build.sh
+++ recipe/build.sh
@@ -57,7 +57,7 @@ python ./configure \
   --with-debugging=0 \
   --with-blas-lib=libblas${SHLIB_EXT} \
   --with-lapack-lib=liblapack${SHLIB_EXT} \
-  --with-hdf5-lib=[libhdf5${SHLIB_EXT}, libhdf5_hl${SHLIB_EXT}] \
+  --with-hdf5=1 \
   --with-hwloc=0 \
   --with-hypre=1 \
   --with-metis=1 \
